8e2035de88ebe3114aedba3ea78570e70817cb23
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2019-10-28 15:59:22 -0300

    first commit

---
+++ app/src/main/java/com/example/servicosautonomos/Escolha.java
@@ -0,0 +1,14 @@
+package com.example.servicosautonomos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Escolha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolha);
+    }
+}
---
+++ app/src/main/java/com/example/servicosautonomos/FormJaCadastrado.java
@@ -0,0 +1,14 @@
+package com.example.servicosautonomos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormJaCadastrado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_ja_cadastrado);
+    }
+}
