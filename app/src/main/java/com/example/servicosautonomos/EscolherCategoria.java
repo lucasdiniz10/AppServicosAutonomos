3d481cb69633f47be28de271b102e9e00f3d894c
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2019-10-31 23:15:58 -0300

    add tela categoria

---
+++ app/src/main/java/com/example/servicosautonomos/EscolherCategoria.java
@@ -0,0 +1,14 @@
+package com.example.servicosautonomos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EscolherCategoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolher_categoria);
+    }
+}
--- app/src/main/java/com/example/servicosautonomos/FormCadastroContratante.java
+++ app/src/main/java/com/example/servicosautonomos/FormCadastroContratante.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class formCadastroContratante extends AppCompatActivity {
+public class FormCadastroContratante extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
