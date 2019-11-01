c0d752da7a341451f64e80f7c34ae385fe5d224f
Lucas Diniz <lucasalvarengadiniz@gmai.com>
2019-11-01 11:03:05 -0300

    em sala

---
+++ app/src/main/java/com/example/servicosautonomos/ActivityPrincipal.java
@@ -0,0 +1,13 @@
+package com.example.servicosautonomos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityPrincipal extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/example/servicosautonomos/MainActivity.java
+++ app/src/main/java/com/example/servicosautonomos/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.servicosautonomos;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_escolher_categoria);new Handler().postDelayed(new Runnable() {
+            /*
+             * Exibindo splash com um timer.
+             */
+            @Override
+            public void run() {
+                // Esse método será executado sempre que o timer acabar
+                // E inicia a activity principal
+                Intent i = new Intent(MainActivity.this,
+                        Escolha.class);
+                startActivity(i);
+
+                // Fecha esta activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
+
+
+
 }
