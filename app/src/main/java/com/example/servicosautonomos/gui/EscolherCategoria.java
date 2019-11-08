a4ba019e7a98913e5a36c72a57992230306f534f
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2019-11-08 03:18:17 -0300

    tela de assist tecnica

--- app/src/main/java/com/example/servicosautonomos/gui/EscolherCategoria.java
+++ app/src/main/java/com/example/servicosautonomos/gui/EscolherCategoria.java
@@ -1,6 +1,9 @@
 package com.example.servicosautonomos.gui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +11,21 @@ import com.example.servicosautonomos.R;
 
 public class EscolherCategoria extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escolher_categoria);
+
+        button = (Button)findViewById(R.id.buttonEscolherCategoriaAssistenciaTecnica);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EscolherCategoria.this, EscolherSubCategoriaAssistenciaTecnica.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
