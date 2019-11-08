e2648427e9ab90c724996d504aee6720d7ee7a99
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2019-11-08 17:56:43 -0300

    Tela Cadastro Profissional

---
+++ app/src/main/java/com/example/servicosautonomos/gui/FormCadastroProfissional.java
@@ -0,0 +1,16 @@
+package com.example.servicosautonomos.gui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.servicosautonomos.R;
+
+public class FormCadastroProfissional extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro_profissional);
+    }
+}
