963d17b16a5fd0b7eb45d2837d165317249112f5
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2019-11-08 02:52:42 -0300

    subcategoria 1 assist tecnica

---
+++ app/src/main/java/com/example/servicosautonomos/gui/EscolherSubCategoriaAssistenciaTecnica.java
@@ -0,0 +1,16 @@
+package com.example.servicosautonomos.gui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.servicosautonomos.R;
+
+public class EscolherSubCategoriaAssistenciaTecnica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolher_sub_categoria_assistencia_tecnica);
+    }
+}
