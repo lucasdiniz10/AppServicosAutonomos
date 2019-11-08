a3df9f997093e6966dd6807a626245f1b37d01ab
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2019-11-08 16:37:35 -0300

    Tela PerfilContratante

---
+++ app/src/main/java/com/example/servicosautonomos/gui/PerfilContratante.java
@@ -0,0 +1,16 @@
+package com.example.servicosautonomos.gui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.servicosautonomos.R;
+
+public class PerfilContratante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_contratante);
+    }
+}
