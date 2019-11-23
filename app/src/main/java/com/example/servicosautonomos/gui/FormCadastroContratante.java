074a586e3bb9e7cf733c0e64c75441265a8f1868
gsenaa <47890401+gsenaa@users.noreply.github.com>
2019-11-23 01:08:04 -0300

    arrumando para o banco

--- app/src/main/java/com/example/servicosautonomos/gui/FormCadastroContratante.java
+++ app/src/main/java/com/example/servicosautonomos/gui/FormCadastroContratante.java
@@ -12,13 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.servicosautonomos.R;
 import com.example.servicosautonomos.classesbasicas.Contratante;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class FormCadastroContratante extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
     private static final String TAG = "Contratante";
     Button buttonLogin;
 
