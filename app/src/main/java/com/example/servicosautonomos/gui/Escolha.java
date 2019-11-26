88eb2e5b2e13c827bc5552b8fdcd2910fc0461f5
Lucas Diniz <lucasalvarengadiniz@gmai.com>
2019-11-26 09:46:26 -0300

    tirando contratante

--- app/src/main/java/com/example/servicosautonomos/gui/Escolha.java
+++ app/src/main/java/com/example/servicosautonomos/gui/Escolha.java
@@ -1,10 +1,11 @@
 package com.example.servicosautonomos.gui;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.servicosautonomos.R;
 
@@ -22,7 +23,7 @@ public class Escolha extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Escolha.this, FormCadastroContratante.class);
+                Intent intent = new Intent(Escolha.this, ListaDeServicos.class);
                 startActivity(intent);
             }
         });
