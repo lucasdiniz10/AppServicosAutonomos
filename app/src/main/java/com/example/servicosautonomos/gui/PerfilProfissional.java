0b2a2f103529aace224008207c785d072c788b7e
gsenaa <47890401+gsenaa@users.noreply.github.com>
2019-11-28 21:09:18 -0300

    Respeita minha historia

--- app/src/main/java/com/example/servicosautonomos/gui/PerfilProfissional.java
+++ app/src/main/java/com/example/servicosautonomos/gui/PerfilProfissional.java
@@ -1,7 +1,11 @@
 package com.example.servicosautonomos.gui;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +16,14 @@ import com.example.servicosautonomos.classesbasicas.Profissional;
 public class PerfilProfissional extends AppCompatActivity {
 
     TextView nome, telefone, endereco, dataNasc, email, servicoOferecido, descricao, dinheiro, cartaoCredito, cartaoDebito;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_profissional);
 
-        Profissional profissional = getIntent().getExtras().getParcelable("dados");
+        final Profissional profissional = getIntent().getExtras().getParcelable("dados");
 
         nome = findViewById(R.id.textViewNome);
         telefone = findViewById(R.id.textViewTelefone);
@@ -51,8 +56,25 @@ public class PerfilProfissional extends AppCompatActivity {
             cartaoCredito.setTextColor(Color.parseColor("#cecece"));
         }
 
+        button = findViewById(R.id.buttonFinalizarCadastro);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
 
 
+                String mEmailAddress = profissional.email;
+                String mPhoneNumber = profissional.telefone;
+
+                intent.putExtra(ContactsContract.Intents.Insert.EMAIL, mEmailAddress)
+                        .putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                        .putExtra(ContactsContract.Intents.Insert.PHONE, mPhoneNumber)
+                        .putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_WORK);
+                startActivity(intent);
+            }
+        });
 
     }
 }
