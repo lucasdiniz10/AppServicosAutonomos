f85e927f33abe9162aad8bbca6fcf5754827b009
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2019-11-29 01:13:19 -0300

    tela editar perfil

---
+++ app/src/main/java/com/example/servicosautonomos/gui/EditarPerfilProfissional.java
@@ -0,0 +1,16 @@
+package com.example.servicosautonomos.gui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.servicosautonomos.R;
+
+public class EditarPerfilProfissional extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil_profissional);
+    }
+}
